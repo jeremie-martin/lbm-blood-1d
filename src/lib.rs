95d581a5fa4172f90d50890a2036a07ecafba0cf
Martin Jérémie <martin.jeremie42@gmail.com>
2020-10-18 22:47:40 +0200

    Add lib.rs documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,9 @@
 #![warn(missing_docs)]
 #![warn(missing_doc_code_examples)]
 
+//! One-dimensional blood flow dynamics simulation using the Lattice Boltzmann Method
+
 pub mod logging;
 pub mod settings;
 pub mod simulation;
 pub mod vessels;
-
-use color_eyre::eyre::{self, WrapErr};
-use tracing::{event, info, instrument, span, warn, Level};
-
-#[cfg(test)]
-mod tests {
-    use indoc::indoc;
-    use pretty_assertions::{assert_eq, assert_ne};
-
-    use super::*;
-}
