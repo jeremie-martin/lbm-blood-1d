d8cd42bb1dfcd8cc3af6e33e35b93f1ec3327daf
Martin Jérémie <martin.jeremie42@gmail.com>
2020-11-18 23:08:36 +0100

    Read OpenBF simulation and ompare two simulations

--- plot.py
+++ plot.py
@@ -3,36 +3,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import json
+import scipy.interpolate as interpolate
 
 plt.style.use(['science', 'grid'])
 
-results_path = 'res'
 
 class Simulation:
-    def __init__(self, path, quantities):
+    def __init__(self, path, quantities, openbf=False):
         if not path.endswith('/'):
             path += '/'
         with open(path + 'metadata.json') as f:
             data = json.load(f)
         vessels_infos = data['vessels_info']
-        self.dx = data['dx']
         self.save_nb = data['save_nb']
         self.time_simul = data['total_time']
         self.path = path
         self.x = np.linspace(0.0, self.time_simul, self.save_nb, endpoint=True)
+        self.dx = data['dx']
         self.vessels = []
 
         for name,length in vessels_infos:
-            self.vessels.append(Vessel(path, name, length, self.save_nb, quantities))
+            self.vessels.append(Vessel(path, name, length, self.save_nb, quantities, self.x, openbf=openbf))
+
+        self.dx = self.vessels[0].length / self.vessels[0].data[quantities[0]].shape[1]
 
     def post_process(self, quantity_to_process, *funcs, **kwargs):
         for v in self.vessels:
             v.post_process(quantity_to_process, *funcs, dx=self.dx, **kwargs)
 
-    def show_quantity(self, quantity_to_plot, name, xlabel='Time ($s$)', ylabel='', title=None):
+    def plot_quantity(self, quantity_to_plot, name, xlabel='Time ($s$)', ylabel='', title=None):
         fig, ax = plt.subplots(figsize=(16,9))
 
-        self.plot_quantity(ax, quantity_to_plot)
+        for v in self.vessels:
+            for quantity,y in v.data_processed.items():
+                if quantity != quantity_to_plot:
+                    continue
+                label = '{} ({})'.format(v.name, quantity)
+                ax.plot(self.x, y, label=label, linewidth=2)
 
         ax.legend(fontsize=15)
         plt.xlabel(xlabel, fontsize=15)
@@ -42,23 +49,49 @@ class Simulation:
 
         fig.savefig(self.path + name, bbox_inches='tight')
 
-    def plot_quantity(self, ax, quantity_to_plot):
-        for v in self.vessels:
-            for quantity,y in v.data_processed.items():
-                if quantity != quantity_to_plot:
-                    continue
-                label = '{} ({})'.format(v.name, quantity)
-                ax.plot(self.x, y, label=label, linewidth=2)
-    
+    def plot_quantity_compare(self, sim, ind, quantity_to_plot, name, xlabel='Time ($s$)', ylabel='', title=None):
+        fig, ax = plt.subplots(figsize=(16,9))
+
+        #  for v in self.vessels:
+        v = self.vessels[ind]
+        for quantity,y in v.data_processed.items():
+            if quantity != quantity_to_plot:
+                continue
+            label = '{} ({}) 1'.format(v.name, quantity)
+            ax.plot(self.x, y, label=label, linewidth=2)
+
+        v = sim.vessels[ind]
+        for quantity,y in v.data_processed.items():
+            if quantity != quantity_to_plot:
+                continue
+            label = '{} ({}) openbf'.format(v.name, quantity)
+            ax.plot(sim.x, y, label=label, linewidth=2)
+
+        ax.legend(fontsize=15)
+        plt.xlabel(xlabel, fontsize=15)
+        plt.ylabel(ylabel, fontsize=15)
+        if title != None:
+            plt.title(title, fontsize=18)
+
+        fig.savefig(self.path + name, bbox_inches='tight')
+
 class Vessel:
-    def __init__(self, path, name, length, save_nb, quantities):
+    def __init__(self, path, name, length, save_nb, quantities, x, openbf=False):
         self.name = name
         self.length = length
         self.data = {}
         self.data_processed = {}
+
         for quantity in quantities:
-            y = np.fromfile(path + name + '_' + quantity)
-            y = np.reshape(y, (save_nb, y.shape[0] // save_nb))
+            if openbf:
+                y = np.loadtxt(path + name + '_' + quantity)
+                oldx = y[:,0]
+                y = y[:,1:]
+                f = interpolate.interp1d(oldx, y, axis=0, kind='cubic')
+                y = f(x)
+            else:
+                y = np.fromfile(path + name + '_' + quantity)
+                y = np.reshape(y, (save_nb, y.shape[0] // save_nb))
             self.data[quantity] = y
 
     def post_process(self, quantity_to_process, *funcs, **kwargs):
@@ -90,16 +123,34 @@ def difference(data, **kwargs):
 def relative_change(data, **kwargs):
     return difference(data) / data[0]
 
+results_path = 'res'
+sim = Simulation(results_path, ['A', 'u'], openbf=False)
 
+#  results_path = 'res_openbf'
+#  sim_openbf = Simulation(results_path, ['A', 'u'], openbf=True)
 
-sim = Simulation(results_path, ['A', 'u'])
+#  results_path = 'res2'
+#  sim_openbf = Simulation(results_path, ['A', 'u'], openbf=False)
 
-sim.post_process('A', get_data_at_position, difference, pos=2.0)
-sim.show_quantity('A', 'A_diff.png', ylabel=r'$A - A_0$', title='A, x = 2m')
+sim.post_process('A', get_data_at_position, pos=0.2)
+#  sim_openbf.post_process('A', get_data_at_position, pos=0.2)
+sim.plot_quantity('A', 'u_diff6.png', ylabel=r'$A - A_0$', title='A, x = 6m')
 
-sim.post_process('A', get_data_at_position, relative_change, pos=4.0)
-sim.show_quantity('A', 'A_relative.png', ylabel=r'$\frac{A - A_0}{A_0}$', title='A, x = 4m')
+#  sim.plot_quantity_compare(sim_openbf, 0, 'A', 'u_diff6.png', ylabel=r'$A - A_0$', title='A, x = 6m')
+#
+#  sim.post_process('A', get_data_at_position, pos=2.0)
+#  sim_openbf.post_process('A', get_data_at_position, pos=2.0)
+#  sim.plot_quantity_compare(sim_openbf, 0, 'A', 'u_diff.png', ylabel=r'$A - A_0$', title='A, x = 2m')
+#
+#  sim.post_process('A', get_data_at_position, pos=0.0)
+#  sim_openbf.post_process('A', get_data_at_position, pos=0.0)
+#  sim.plot_quantity_compare(sim_openbf, 0, 'A', 'A_diff.png', ylabel=r'$A - A_0$', title='A, x = 0m')
 
-sim.post_process('u', get_data_at_position, pos=2.0)
-sim.show_quantity('u', 'u.png', ylabel=r'$u$', title='u, x = 2m')
+#  sim.post_process('A', get_data_at_position, relative_change, pos=4.0)
+#  sim_openbf.post_process('A', get_data_at_position, relative_change, pos=4.0)
+#  sim.plot_quantity_compare(sim_openbf, 'A', 'A_relative.png', ylabel=r'$\frac{A - A_0}{A_0}$', title='A, x = 4m')
+#
+#  sim.post_process('u', get_data_at_position, pos=2.0)
+#  sim_openbf.post_process('u', get_data_at_position, pos=2.0)
+#  sim.plot_quantity_compare(sim_openbf, 'u', 'u.png', ylabel=r'$u$', title='u, x = 2m')
 
--- vid.py
+++ vid.py
@@ -3,23 +3,42 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 from scipy.ndimage.filters import gaussian_filter1d
 from matplotlib import animation
+import scipy.interpolate as interpolate
 
 #  plt.rcParams['animation.ffmpeg_path'] = ''
 
 # y = np.fromfile('out/A')
 # y2 = np.fromfile('out/A')
 # y3 = np.fromfile('out/A')
-SAVE_NB = 3301
+SAVE_NB = 657
 
-y = np.fromfile('res/A_A')
-y2 = np.fromfile('res/B_A')
-y3 = np.fromfile('res/C_A')
-y = np.reshape(y, (SAVE_NB, y.shape[0] // SAVE_NB))
+#  y = np.fromfile('res/A_A')
+#  y2 = np.fromfile('res/A_A')
+#  y3 = np.fromfile('res/A_A')
+#  y = np.reshape(y, (SAVE_NB, y.shape[0] // SAVE_NB))
+#  y2 = np.reshape(y2, (SAVE_NB, y2.shape[0] // SAVE_NB))
+#  y3 = np.reshape(y3, (SAVE_NB, y3.shape[0] // SAVE_NB))
+
+y = np.loadtxt('res_openbf/A_AA')
+y2 = np.loadtxt('res_openbf/A_P')
+y3 = np.loadtxt('res_openbf/A_AA')
+x = y[:,0]
+x2 = y2[:,0]
+x3 = y3[:,0]
+y = y[:,1:]
+y2 = y2[:,1:]
+y3 = y3[:,1:]
+
+f = interpolate.interp1d(x, y, axis=0, kind='linear')
+f2 = interpolate.interp1d(x2, y2, axis=0, kind='cubic')
+f3 = interpolate.interp1d(x3, y3, axis=0, kind='linear')
+
+x = np.linspace(0.0, 3.3, SAVE_NB, endpoint=True)
+y = f(x)
+y2 = np.fromfile('res/A_A')
 y2 = np.reshape(y2, (SAVE_NB, y2.shape[0] // SAVE_NB))
-y3 = np.reshape(y3, (SAVE_NB, y3.shape[0] // SAVE_NB))
+y3 = f3(x)
 
-print(y[0:5])
-print(y[-5:])
 #  y = np.fromfile('res/A')
 #  y2 = np.fromfile('res/A')
 #  y3 = np.fromfile('res/A')
@@ -51,13 +70,10 @@ print(y3.shape)
 # print((y2-y3).sum().sum())
 
 # print(y[0])
-from scipy import interpolate
 
 #  y = y / y[0]
 #  y2 = y2 / y2[0]
 #  y3 = y3 / y3[0]
-
-tot = len(y[0]) + len(y2[0]) + len(y3[0])
 
 FRAME_NB = y.shape[0]
 
@@ -66,15 +82,19 @@ FRAME_NB = y.shape[0]
 #  y3 = y3 - y3[0]
 
 fig, ax = plt.subplots(figsize=(16, 9))
-x = np.linspace(0, 3, len(y[0]))
-x2 = np.linspace(3, 6, len(y2[0]))
-x3 = np.linspace(6, 9, len(y3[0]))
+x = np.linspace(0, 8.6, len(y[0]))
+x2 = np.linspace(8.6, 8.6+8.5, len(y2[0]))
+x3 = np.linspace(8.6 + 8.5, 8.6+8.5+8.5, len(y3[0]))
 line, = ax.plot(x, np.sin(x / 100), 'b', label='A')
 line2, = ax.plot(x2, np.sin(x2 / 100), 'r', label='B')
 line3, = ax.plot(x3, np.sin(x3 / 100), 'g')
 plt.legend()
 time_text = ax.text(0.05, 0.95, '', horizontalalignment='left',
                     verticalalignment='top', transform=ax.transAxes)
+
+print(x[0], x[-1])
+print(x2[0], x2[-1])
+print(x3[0], x3[-1])
 
 print(np.min(np.min(y)))
 print(np.max(np.max(y)))
